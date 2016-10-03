727a01b7592d61c7ab47738805bf59a9e477d924
Raphael Puccinelli <raphaelpuccinelli@gmail.com>
2016-10-03 23:44:19 +0200

    setting up back end java

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import clientConnection.Client;
+import subscriber.SmartMirror_Subscriber;
+
+public class test
+{
+    public static void main(String[] args)
+    {
+        Client client = new Client("tcp://codehigh.ddns.net:1883", "Tester");
+        new SmartMirror_Subscriber(client, "test");
+    }
+}
