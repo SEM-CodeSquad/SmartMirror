67f41821f3496ac0c7a6f3536761be7293cb5ec5
CYuZeric <chyj1993@outlook.com>
2016-10-13 10:53:04 +0200

    comment and test package delete

--- src/clientConnection/Client.java
+++ src/clientConnection/Client.java
@@ -2,13 +2,9 @@ package clientConnection;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
-
 import java.util.concurrent.TimeUnit;
-
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -19,14 +15,22 @@ public class Client
     private ScheduledExecutorService scheduler;
    private MqttClient client;
 
+
+//Create a new client with set URL and client ID with options set.
     public Client(String url, String id)
     {
         try {
+            //Options set for the MQTT connections
             options = new MqttConnectOptions();
+            //set the server to keep a log of connection activities
             options.setCleanSession(false);
+            //set the connection time out to 10
             options.setConnectionTimeout(10);
+            //client will send a heart beat to the server to keep itself alive, it is set to 20*1.5 sec.
             options.setKeepAliveInterval(20);
+            //create a new client and set the URL and the ID
             client = new MqttClient(url, id);
+            //connect the client to the server with options
             client.connect(options);
 
             System.out.println("Client Connected!");
@@ -42,14 +46,14 @@ public class Client
             e.printStackTrace();
         }
     }
-//TODO we might need to figure out a way to do this...
+
+//The reconnection functions. as a new single threaded scheduled.
     public void startReconnect() {
         scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduler.scheduleAtFixedRate(new Runnable() {
             public void run() {
                 if (!client.isConnected()) {
                     try {
-
                         client.connect(options);
                     } catch (MqttSecurityException e) {
                         e.printStackTrace();
@@ -61,7 +65,7 @@ public class Client
         }, 0 * 1000, 10 * 1000, TimeUnit.MILLISECONDS);
     }
 
-
+//Client getter, it returns a client object.
     public MqttClient getClient()
     {
         return client;
