f4798f70d5d2fdf56512694ad653153652590a93
CYuZeric <chyj1993@outlook.com>
2016-11-04 14:56:19 +0100

    RSS Parser test

---
+++ src/widgets/testRSS.java
@@ -0,0 +1,16 @@
+package widgets;
+
+/**
+ * Created by Geoffrey on 2016/11/4.
+ */
+
+public class testRSS {
+    public static void main(String[] args) {
+        RSSStAXParser parser = new RSSStAXParser("https://www.reddit.com/r/news/.rss");
+        RSSFeed feed = parser.RSSParser();
+        System.out.println(feed);
+        for (Object message : feed.getList()) {
+            System.out.println(message);
+        }
+    }
+}
