d8bc3b89aa0e4d64e952eed5da4b38937c721dda
Axel <axel@verner.se>
2016-11-04 20:22:18 +0100

    Added resources on TimeTable

--- src/widgets/BusTimetable.java
+++ src/widgets/BusTimetable.java
@@ -12,4 +12,27 @@ public class BusTimetable
         web.load("http://www.vasttrafik.se/nasta-tur-fullskarm/?externalid=9021014004945000&friendlyname="
                 + busStop + "+G%C3%B6teborg");
     }
+
+
+    //TODO Notes ->
+
+    // TODO GENERATE TOKEN:
+
+    // POST https://api.vasttrafik.se/token HTTP/1.1
+    // Content-Type: application/x-www-form-urlencoded
+    // Authorization: Basic UmJseEkyeTFsWVNFTTZ0Z2J6anBTa2E0R1o6Wk1nSkR0Y0paRGV4OTJldUxpQUdYOFExUnU=
+    // grant_type=client_credentials&scope=<device_id>   --------(device_id needs to be specified)-------
+
+    // TODO Example link that returns a json object containing departures:
+    // NOTE: Needs authentication token, done through Oauth2
+
+    // https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=Brunnsparken%2CG%C3%B6teborg&date=2016-11-04&time=19%3A22&format=json
+
+    // TODO Fetch the json object with the auth code:
+
+    // GET https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=ols&format=json HTTP/1.1
+    // Authorization: Bearer 766b697f4462ca3878b05597ff19313
+
+    // TODO Here's a link to git where some dude made it work:
+    // https://github.com/vasttrafik/wso2-apim-developer-portal-api/tree/master/java/src
 }
