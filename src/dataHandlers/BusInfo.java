2ad5f2e6d1f3ee917f7ec888992a4e302d838018
Axel <axel@verner.se>
2016-11-12 12:19:11 +0100

    BusData implemented + Some clean-up Fixed a problem,BusInfo is now added.

---
+++ src/dataHandlers/BusInfo.java
@@ -0,0 +1,24 @@
+package dataHandlers;
+
+/**
+ * Created by Axel on 11/10/2016.
+ */
+public class BusInfo {
+    public String busFrom;
+    public String busDirection;
+    public String busName;
+    public String busDeparture;
+
+    public String getBusFrom(){
+        return busFrom;
+    }
+    public String getBusDirection() {
+        return busDirection;
+    }
+    public String getBusName() {
+        return busName;
+    }
+    public String getBusDeparture(){
+        return busDeparture;
+    }
+}
