3c112edeef5cc2f0c017b932dc36d9bf03bc598e
Raphael Puccinelli <raphaelpuccinelli@gmail.com>
2016-12-16 02:10:48 +0100

    Update

--- src/smartMirror/main/Main.java
+++ src/smartMirror/main/Main.java
@@ -25,9 +25,11 @@
 package smartMirror.main;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
